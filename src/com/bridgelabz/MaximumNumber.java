856f9f4e0cda13210a014aa93c16ab65160a2625
manjukalal <manjukalal1496@gmail.com>
2022-06-18 18:34:47 +0530

    Added:finding maximum values of integer and floating numbers

--- src/com/bridgelabz/MaximumNumber.java
+++ src/com/bridgelabz/MaximumNumber.java
@@ -14,9 +14,25 @@ public class MaximumNumber {
         return max;
     }
 
+    public static Float maximumFloat(Float a, Float b, Float c) {
+
+        Float max = a;
+
+        if (b.compareTo(max) > 0) {
+            max = b;
+        } else if (c.compareTo(max) > 0) {
+            max = c;
+
+        }
+        return max;
+    }
+
+
     public static void main(String[] args) {
         Integer result = MaximumNumber.maximum(7, 9, 8);
-        System.out.println("maximum number is: " + result);
+        System.out.println("maximum integer number is: " + result);
+        Float  result1 = MaximumNumber.maximumFloat(6.7f,5.3f,6.1f);
+        System.out.println("maximum floating number is: " + result1);
     }
 
 }
