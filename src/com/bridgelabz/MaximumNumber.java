3472609fb642f56e13d94b45a6e2bc5454984643
manjukalal <manjukalal1496@gmail.com>
2022-06-18 23:17:36 +0530

    Modified:code by using sorting

--- src/com/bridgelabz/MaximumNumber.java
+++ src/com/bridgelabz/MaximumNumber.java
@@ -1,36 +1,17 @@
 package com.bridgelabz;
-
+import java.util.Arrays;
 public class MaximumNumber {
+    public static <T> T maximum(T... a) {
+        Arrays.sort(a);
+        return a[a.length - 1];
+    }
+
     public static void main(String[] args) {
-      Genericdata<Integer> genericdata = new Genericdata<> (3,5,6);
-        System.out.println("maximum integer number is: " +genericdata.maximum() );
-        Genericdata<Float> genericdata1 = new Genericdata<>  (6.7f, 5.3f, 6.1f);
-        System.out.println("maximum floating number is: " + +genericdata1.maximum());
-        Genericdata<String> genericdata2 = new Genericdata<> ("apple", "banana", "jackfruit");
-        System.out.println("length of given string is: " +genericdata2.maximum());
+        System.out.println("maximum integer number is: " + maximum(3, 6, 1, 13, 56, 18));
+        System.out.println("maximum flot number is: " + maximum(1.2f, 5.6f, 7.6f, 2.2f, 2.2f));
+        System.out.println("maximum value string name is: " + maximum("apple", "banana","coconut" ,"jackfruit", "grapes"));
     }
-
 }
-   class Genericdata <T extends Comparable > {
-        T a,b,c;
-
-       public Genericdata(T a, T b, T c) {
-           this.a = a;
-           this.b = b;
-           this.c = c;
-       }
-
-       public T maximum() {
-        T max = a;
-        if (b.compareTo(max) > 0) {
-            max = b;
-        }
-        if (c.compareTo(max) > 0) {
-            max = c;
-        }
-        return max;
-    }
-        }
 
 
 
