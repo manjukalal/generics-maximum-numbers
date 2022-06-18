b63a9940a69c322cca3320c7710636ed5d94e584
manjukalal <104911815+manjukalal@users.noreply.github.com>
2022-06-18 23:43:44 +0530

    Update MaximumNumber.java

--- src/com/bridgelabz/MaximumNumber.java
+++ src/com/bridgelabz/MaximumNumber.java
@@ -2,19 +2,19 @@ package com.bridgelabz;
 
 public class MaximumNumber {
     public static void main(String[] args) {
-      Genericdata<Integer> genericdata = new Genericdata<> (3,5,6);
+      GenericData<Integer> genericdata = new GenericData<> (3,5,6);
         System.out.println("maximum integer number is: " +genericdata.maximum() );
-        Genericdata<Float> genericdata1 = new Genericdata<>  (6.7f, 5.3f, 6.1f);
-        System.out.println("maximum floating number is: " + +genericdata1.maximum());
-        Genericdata<String> genericdata2 = new Genericdata<> ("apple", "banana", "jackfruit");
+        GenericData<Float> genericdata1 = new GenericData<>  (6.7f, 5.3f, 6.1f);
+        System.out.println("maximum floating number is: "  +genericdata1.maximum());
+        GenericData<String> genericdata2 = new GenericData<> ("apple", "banana", "jackfruit");
         System.out.println("length of given string is: " +genericdata2.maximum());
     }
 
 }
-   class Genericdata <T extends Comparable > {
+   class GenericData <T extends Comparable > {
         T a,b,c;
 
-       public Genericdata(T a, T b, T c) {
+       public GenericData(T a, T b, T c) {
            this.a = a;
            this.b = b;
            this.c = c;
