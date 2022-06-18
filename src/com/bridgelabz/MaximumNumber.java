8a5702bedfb006f66230ebf576224174913564d8
manjukalal <manjukalal1496@gmail.com>
2022-06-18 22:59:24 +0530

    Refactor:code using constructor

--- src/com/bridgelabz/MaximumNumber.java
+++ src/com/bridgelabz/MaximumNumber.java
@@ -1,8 +1,26 @@
 package com.bridgelabz;
 
 public class MaximumNumber {
+    public static void main(String[] args) {
+      Genericdata<Integer> genericdata = new Genericdata<> (3,5,6);
+        System.out.println("maximum integer number is: " +genericdata.maximum() );
+        Genericdata<Float> genericdata1 = new Genericdata<>  (6.7f, 5.3f, 6.1f);
+        System.out.println("maximum floating number is: " + +genericdata1.maximum());
+        Genericdata<String> genericdata2 = new Genericdata<> ("apple", "banana", "jackfruit");
+        System.out.println("length of given string is: " +genericdata2.maximum());
+    }
 
-    public static <T extends Comparable> T maximum(T a, T b, T c) {
+}
+   class Genericdata <T extends Comparable > {
+        T a,b,c;
+
+       public Genericdata(T a, T b, T c) {
+           this.a = a;
+           this.b = b;
+           this.c = c;
+       }
+
+       public T maximum() {
         T max = a;
         if (b.compareTo(max) > 0) {
             max = b;
@@ -12,14 +30,8 @@ public class MaximumNumber {
         }
         return max;
     }
+        }
 
-    public static void main(String[] args) {
-        Integer result = MaximumNumber.maximum(7, 9, 8);
-        System.out.println("maximum integer number is: " + result);
-        Float result1 = MaximumNumber.maximum(6.7f, 5.3f, 6.1f);
-        System.out.println("maximum floating number is: " + result1);
-        String result2 = MaximumNumber.maximum("apple", "banana", "jackfruit");
-        System.out.println("length of given string is: " + result2);
-    }
 
-}
+
+
